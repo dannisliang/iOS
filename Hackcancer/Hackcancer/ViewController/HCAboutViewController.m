fdb01f0b90bb995d73a3b8e64add35c3696f42da
James Campbell <jscampbell.05@googlemail.com>
2015-08-01 00:28:19 +0100

    Xibs register code.

--- Hackcancer/Hackcancer/ViewController/HCAboutViewController.m
+++ Hackcancer/Hackcancer/ViewController/HCAboutViewController.m
@@ -21,10 +21,36 @@
 
 @implementation HCAboutViewController
 
+static NSString * const HCAboutMapIdentifier = @"AboutMapIdentifier";
+static NSString * const HCAboutDescriptionIdentifier = @"AboutDescriptionIdentifier";
+static NSString * const HCAboutDateIdentifier = @"AboutDateIdentifier";
+static NSString * const HCAboutSocialNetworkIdentifier = @"AboutSocialNetwork";
+
 #pragma mark - View Controller Lifecycle
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    UINib *tableViewCellNib = [UINib nibWithNibName:HCAboutMapIdentifier
+                                             bundle:[NSBundle mainBundle]];
+    [self.tableView registerNib:tableViewCellNib
+         forCellReuseIdentifier:HCAboutMapIdentifier];
+    
+    UINib *tableViewCellNib2 = [UINib nibWithNibName:HCAboutDescriptionIdentifier
+                                              bundle:[NSBundle mainBundle]];
+    [self.tableView registerNib:tableViewCellNib2
+         forCellReuseIdentifier:HCAboutDescriptionIdentifier];
+    
+    UINib *tableViewCellNib3 = [UINib nibWithNibName:HCAboutDateIdentifier
+                                              bundle:[NSBundle mainBundle]];
+    [self.tableView registerNib:tableViewCellNib3
+         forCellReuseIdentifier:HCAboutDateIdentifier];
+    
+    UINib *tableViewCellNib4 = [UINib nibWithNibName:HCAboutSocialNetworkIdentifier
+                                              bundle:[NSBundle mainBundle]];
+    [self.tableView registerNib:tableViewCellNib4
+         forCellReuseIdentifier:HCAboutSocialNetworkIdentifier];
 }
+
 @end
