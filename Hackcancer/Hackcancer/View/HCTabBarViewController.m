c053e9ba3f71d98ef318c05ac6e1e6a2a988dac0
Rob Swift <rob@hackcancer.co>
2015-08-01 01:27:51 +0100

    Added tab bar item.

--- Hackcancer/Hackcancer/View/HCTabBarViewController.m
+++ Hackcancer/Hackcancer/View/HCTabBarViewController.m
@@ -13,6 +13,7 @@
 @interface HCTabBarViewController ()
 
 @property (nonatomic, strong) HCUserInterface *userInterface;
+@property (nonatomic, strong) UIImage *tabBarItem0;
 
 @end
 
@@ -20,20 +21,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    
-//    self.userInterface.
-//    
-//    UIImage *tabBarItem1 = self.userInterface.newsTabBarItem;
-//    UIImage *tabBarItem2 = self.userInterface.newsTabBarItem;
-//    UIImage *tabBarItem3 = self.userInterface.newsTabBarItem;
-//    UIImage *tabBarItem4 = self.userInterface.newsTabBarItem;
-//    UIImage *tabBarItem5 = self.userInterface.newsTabBarItem;
-//
-//    [[self.tabBarController.tabBar.items objectAtIndex:1] setImage:tabBarItem1];
-//    [[self.tabBarController.tabBar.items objectAtIndex:2] setImage:tabBarItem2];
-//    [[self.tabBarController.tabBar.items objectAtIndex:3] setImage:tabBarItem3];
-//    [[self.tabBarController.tabBar.items objectAtIndex:4] setImage:tabBarItem4];
-//    [[self.tabBarController.tabBar.items objectAtIndex:5] setImage:tabBarItem5];
+    
+    
+    [self.userInterface.newsTabBarItem getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+        if (!error) {
+            self.tabBarItem0 = [UIImage imageWithData:data];
+        }
+    }];
+    
+    [[self.tabBarController.tabBar.items objectAtIndex:0] setImage:self.tabBarItem0];
+    
+//    [[self.tabBarController.tabBar.items objectAtIndex:1] setImage:tabBarItem2];
+//    [[self.tabBarController.tabBar.items objectAtIndex:2] setImage:tabBarItem3];
+//    [[self.tabBarController.tabBar.items objectAtIndex:3] setImage:tabBarItem4];
+//    [[self.tabBarController.tabBar.items objectAtIndex:4] setImage:tabBarItem5];
 }
 
 
